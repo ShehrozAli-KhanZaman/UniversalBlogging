ce25002f0c7d2937b558bb9b42dddb3b27751f87
Shehroz <shehrozalikhanzaman@gmail.com>
2024-04-23 11:03:49 +0500

    youtube app

---
+++ android/app/src/main/kotlin/com/example/universalblogging/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.universalblogging
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,16 @@
 // import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:flutter_inappwebview/flutter_inappwebview.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  MobileAds.instance.initialize();
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -25,7 +28,7 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
 
   final String title;
 
@@ -34,31 +37,64 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  BannerAd? _bannerAd;
+  bool isLoaded = false;
   int progress = 0;
   late InAppWebViewController inAppWebViewController;
+  final adUnitId = 'ca-app-pub-7324394289065470/9895025441';
+
+  @override
+  void initState() {
+    super.initState();
+    _loadAd();
+  }
+
+  _loadAd() {
+    _bannerAd = BannerAd(
+      adUnitId: adUnitId,
+      request: const AdRequest(),
+      size: AdSize.banner,
+      listener: BannerAdListener(
+        onAdLoaded: (ad) {
+          debugPrint('$ad loaded.');
+          setState(() {
+            isLoaded = true;
+          });
+        },
+        onAdFailedToLoad: (ad, err) {
+          debugPrint('BannerAd failed to load: $err');
+          ad.dispose();
+        },
+      ),
+    )..load();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text(
-          'Universal Blogging',
-          style: TextStyle(color: Colors.white),
-        ),
-        backgroundColor: const Color.fromARGB(255, 13, 1, 49),
+        title: _bannerAd != null
+            ? SafeArea(
+                child: SizedBox(
+                  width: _bannerAd!.size.width.toDouble(),
+                  height: _bannerAd!.size.height.toDouble(),
+                  child: AdWidget(ad: _bannerAd!),
+                ),
+              )
+            : null,
       ),
       body: Stack(
         children: [
           InAppWebView(
             initialUrlRequest: URLRequest(
-                url: Uri.parse(
-                    "https://www.youtube.com/")), // https://www.universalblogging.com/
+              url: Uri.parse(
+                  "https://www.youtube.com/")), // https://www.universalblogging.com/
             // onWebViewCreated: (InAppWebViewController controller {
             //   inAppWebViewController = controller;
             // }),
-            onProgressChanged:
-                (InAppWebViewController controller, int progress) =>
-                    {progress = (progress / 100) as int},
+            onProgressChanged: (controller, newProgress) {
+              setState(() {});
+            },
           )
         ],
       ),
